e7247e1eeacc3146301b5afb2566a472d60f5bc1
Lyden CHAI <lyden.chai@gmail.com>
2025-01-16 11:43:11 +0700

    implement login

--- app/src/main/java/com/example/denfristapp/ItemDetailActivity.java
+++ app/src/main/java/com/example/denfristapp/ItemDetailActivity.java
@@ -1,6 +1,9 @@
 package com.example.denfristapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +23,10 @@ public class ItemDetailActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void backToList(View view) {
+        Intent intent = new Intent(ItemDetailActivity.this, ListItemActivity.class);
+        startActivity(intent);
     }
 }
