b31340bed25a022469f41c9cbb651274352ca4bf
Lyden CHAI <lyden.chai@gmail.com>
2025-01-16 09:14:45 +0700

    classwork 5

--- app/src/main/java/com/example/denfristapp/MainActivity.java
+++ app/src/main/java/com/example/denfristapp/MainActivity.java
@@ -1,9 +1,10 @@
 package com.example.denfristapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -14,8 +15,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        // Apply window insets for edge-to-edge support
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -23,39 +25,45 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Toast.makeText(this, "Start", Toast.LENGTH_SHORT).show();
+    public void openDetailActivity(View view) {
+        Intent intent = new Intent(MainActivity.this, DetailActivity.class);
+        startActivity(intent);
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Toast.makeText(this, "Resume", Toast.LENGTH_SHORT).show();
-    }
+//    @Override
+//    protected void onStart() {
+//        super.onStart();
+//        Toast.makeText(this, "Start", Toast.LENGTH_SHORT).show();
+//    }
+//
+//    @Override
+//    protected void onResume() {
+//        super.onResume();
+//        Toast.makeText(this, "Resume", Toast.LENGTH_SHORT).show();
+//    }
+//
+//    @Override
+//    protected void onPause() {
+//        super.onPause();
+//        Toast.makeText(this, "Pause", Toast.LENGTH_SHORT).show();
+//    }
+//
+//    @Override
+//    protected void onStop() {
+//        super.onStop();
+//        Toast.makeText(this, "Stop", Toast.LENGTH_SHORT).show();
+//    }
+//
+//    @Override
+//    protected void onRestart() {
+//        super.onRestart();
+//        Toast.makeText(this, "Restart", Toast.LENGTH_SHORT).show();
+//    }
+//
+//    @Override
+//    protected void onDestroy() {
+//        super.onDestroy();
+//        Toast.makeText(this, "Destroyed", Toast.LENGTH_SHORT).show();
+//    }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Toast.makeText(this, "Pause", Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Toast.makeText(this, "Stop", Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        Toast.makeText(this, "Restart", Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Toast.makeText(this, "Destroyed", Toast.LENGTH_SHORT).show();
-    }
 }
